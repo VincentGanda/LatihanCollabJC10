8e65d9a7bf2ea1b40a88809f1834612330d94f4d
Vincent <vincentganda@gmail.com>
2019-08-09 09:45:50 +0700

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("hello world")
 //git init
 //git add itu buat ngepush, dia bisa file apa aja
+// kalo mau push semua file, bisa git add .
+// kalo mau file tertentu kita bisa git add index.js
+//git commit -m "comment yang kita mau"
+//git status itu buat ngeliat
